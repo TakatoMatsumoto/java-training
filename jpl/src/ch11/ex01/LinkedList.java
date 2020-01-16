8a228f429fbd293b1909e74fe7b33e2023a27664
TakatoMatsumoto <takato.matsumoto0114@gmail.com>
2020-01-17 00:46:05 +0900

    edit

--- jpl/src/ch11/ex01/LinkedList.java
+++ jpl/src/ch11/ex01/LinkedList.java
@@ -5,15 +5,15 @@
 
 /*
  * 練習問題2.2 p.37
- * LinkedListクラスを作成しなさい。LinkeListクラスは、Object型のフィールドと、リストの中で次のLinkedList要素への参照を持ちます。
+ * LinkedListクラスを作成しなさい。LinkeListクラスは、Element型のフィールドと、リストの中で次のLinkedList要素への参照を持ちます。
  */
 
 package ch11.ex01;
 
 public class LinkedList<E>
 {
-    E object;
-    LinkedList<E> nextObject;
+    E element;
+    LinkedList<E> nextElement;
 
     /**
      * @param args
@@ -22,11 +22,11 @@ public class LinkedList<E>
     {
 
         LinkedList<String> test1 = new LinkedList<String>();
-        test1.object = "test1";
-        test1.nextObject = new LinkedList<String>();
-        test1.nextObject.object = "test2";
-        System.out.println(test1.object);
-        System.out.println(test1.nextObject.object);
+        test1.element = "test1";
+        test1.nextElement = new LinkedList<String>();
+        test1.nextElement.element = "test2";
+        System.out.println(test1.element);
+        System.out.println(test1.nextElement.element);
 
     }
 
